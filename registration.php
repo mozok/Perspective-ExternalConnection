2535cf1403736cca27ca6193a4220a416a0dfd0f
Mozok Evgen <mozokevgen@gmail.com>
2021-08-10 09:12:54 +0300

    Init module

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright Mozok Evgen
+ * See license.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Perspective_ExternalConnection', __DIR__);
